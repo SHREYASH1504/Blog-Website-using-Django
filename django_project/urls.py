bc2735202a2f4dd9b9faaa68e55b087d25c65cdc
Shreyash Nikhare <shreyashnikhare1504@gmail.com>
2025-09-25 01:19:15 +0530

    Updated Profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 from users.views import MyLogoutView
 
@@ -27,4 +29,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', MyLogoutView.as_view(), name="logout"),
     path('', include('blog.urls')),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
